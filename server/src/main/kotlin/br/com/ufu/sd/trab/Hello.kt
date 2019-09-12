56734d73c0e1305aa0ce8eddd9e9b691e8cfbf62
viniciussousazup <vinicius.sousa@zup.com.br>
2019-09-12 13:36:52 -0300

    server hello world

---
+++ server/src/main/kotlin/br/com/ufu/sd/trab/Hello.kt
@@ -0,0 +1,6 @@
+package br.com.ufu.sd.trab
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ server/src/test/kotlin/br/com/ufu/sd/trab/HelloTest.kt
@@ -0,0 +1,8 @@
+package br.com.ufu.sd.trab
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
